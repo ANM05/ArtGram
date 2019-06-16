4e1f46ca48299a5db9923e14e2cfcdc011aeba39
Antony Nganga <antonynganga05@gmail.com>
2019-06-16 15:10:07 +0300

    Arrange login views

--- app/src/main/java/com/example/artgram/LoginActivity.java
+++ app/src/main/java/com/example/artgram/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.example.artgram;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.email)
+    EditText mLoginEmail;
+    @BindView(R.id.password)
+    EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
     }
 }
