4976736a293d6116303a45e4ec1cb6cfff7dfd57
Antony Nganga <antonynganga05@gmail.com>
2019-06-17 16:05:04 +0300

    Add intent to details activity

--- app/src/main/java/com/example/artgram/DetailsActivity.java
+++ app/src/main/java/com/example/artgram/DetailsActivity.java
@@ -2,7 +2,12 @@ package com.example.artgram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        ImageView fullscreenImgView=(ImageView)findViewById(R.id.imgFullScreen);
+
+        Intent callIntent=getIntent();
+        if(callIntent !=null){
+            Uri imageUri=callIntent.getData();
+            if(imageUri !=null && fullscreenImgView !=null){
+                Glide.with(this).load(imageUri).into(fullscreenImgView);
+            }
+        }
     }
 }
