34f070c4fdc3804e74c2e90ffdf740d7188af171
Antony Nganga <antonynganga05@gmail.com>
2019-06-24 11:27:04 +0300

    Add getters and constructor to model

--- app/src/main/java/com/example/artgram/models/RecentPhotos.java
+++ app/src/main/java/com/example/artgram/models/RecentPhotos.java
@@ -1,4 +1,27 @@
 package com.example.artgram.models;
 
 public class RecentPhotos {
+
+    private String mTitle;
+    private String mImageId;
+    private String mOwnerId;
+
+    public RecentPhotos(String title, String imageId, String ownerId){
+        this.mTitle=title;
+        this.mImageId=imageId;
+        this.mOwnerId=ownerId;
+
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getImageId() {
+        return mImageId;
+    }
+
+    public String getOwnerId() {
+        return mOwnerId;
+    }
 }
