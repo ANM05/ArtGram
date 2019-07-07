aa1e6c6230583d41702bf18d5b389b39579d39c8
Antony Nganga <antonynganga05@gmail.com>
2019-07-07 23:29:15 +0300

    Add signUP functionality

--- app/src/main/java/com/example/artgram/ui/SignUpActivity.java
+++ app/src/main/java/com/example/artgram/ui/SignUpActivity.java
@@ -1,19 +1,45 @@
 package com.example.artgram.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.artgram.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.wang.avi.AVLoadingIndicatorView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.loading)
     AVLoadingIndicatorView loadingIndicatorView;
+    @BindView(R.id.name)
+    EditText names;
+    @BindView(R.id.email)
+    EditText email;
+    @BindView(R.id.password)
+    EditText password;
+    @BindView(R.id.confirm_password)
+    EditText confirm_password;
+    @BindView(R.id.sign_up)
+    Button signUp;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    public static final String TAG = SignUpActivity.class.getSimpleName();
 
 
     @Override
@@ -23,5 +49,53 @@ public class SignUpActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         loadingIndicatorView.hide();
+
+        signUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == signUp) {
+            createNewUser();
+        }
+
+    }
+
+    private void createNewUser() {
+        final String name = names.getText().toString().trim();
+        final String emailInput = email.getText().toString().trim();
+        String passWord = password.getText().toString().trim();
+        String confirmPassword = confirm_password.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(emailInput, passWord)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+//        private void createAuthStateListener () {
+//            mAuthListener = new FirebaseAuth.AuthStateListener() {
+//
+//                @Override
+//                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                    final FirebaseUser user = firebaseAuth.getCurrentUser();
+//                    if (user != null) {
+//                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                }
+//
+//            };
+//        }
     }
 }
+
