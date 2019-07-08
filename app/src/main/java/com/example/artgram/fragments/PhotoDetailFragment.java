dbecfc60351949a4a4b24b8abe50c6e373c6ebc1
Antony Nganga <antonynganga05@gmail.com>
2019-07-08 17:32:39 +0300

    Add save photo functionality

--- app/src/main/java/com/example/artgram/fragments/PhotoDetailFragment.java
+++ app/src/main/java/com/example/artgram/fragments/PhotoDetailFragment.java
@@ -11,9 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.artgram.R;
 import com.example.artgram.models.RecentPhotos;
+import com.example.artgram.services.Constants;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -31,6 +36,8 @@ public class PhotoDetailFragment extends Fragment implements View.OnClickListene
     ImageView mphotoImageView;
     @BindView(R.id.photoDescription)
     TextView mPhotoDescription;
+    @BindView(R.id.btn_fav)
+    FloatingActionButton favouriteBtn;
 
     private RecentPhotos mRecentPhotos;
     private Context context;
@@ -60,17 +67,26 @@ public class PhotoDetailFragment extends Fragment implements View.OnClickListene
         View view = inflater.inflate(R.layout.fragment_photo_detail, container, false);
         ButterKnife.bind(this, view);
         Picasso.get().load(mRecentPhotos.getUrl().getRegular()).into(mphotoImageView);
-//        GlideApp
+        mPhotoDescription.setText(mRecentPhotos.getDescription());
+
+        //        GlideApp
 //                .with(context)
 //                .load(mRecentPhotos.getUrl().getRegular())
 //                .into(mphotoImageView);
-        mPhotoDescription.setText(mRecentPhotos.getDescription());
+        favouriteBtn.setOnClickListener(this);
 
         return view;
     }
 
     @Override
     public void onClick(View v) {
+        if(v==favouriteBtn){
+            DatabaseReference photosRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(Constants.FIREBASE_CHILD_PHOTOGRAPHS);
+            photosRef.push().setValue(mRecentPhotos);
+            Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
