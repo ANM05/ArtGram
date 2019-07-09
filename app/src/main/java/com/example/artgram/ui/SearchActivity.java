a277cd41cf2d171aa1b410a4488e231eb195b6dc
Antony Nganga <antonynganga05@gmail.com>
2019-07-09 15:23:10 +0300

    Add search app bar to search activity

--- app/src/main/java/com/example/artgram/ui/SearchActivity.java
+++ app/src/main/java/com/example/artgram/ui/SearchActivity.java
@@ -1,10 +1,18 @@
 package com.example.artgram.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.example.artgram.R;
+
+import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -12,5 +20,25 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        ButterKnife.bind(this);
+
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
